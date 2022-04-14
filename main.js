ee7c0e6a6e14d683b920136693d39e01d87527ad
PeaceZay <zay.smith007@gmail.com>
2022-04-13 20:13:57 -0500

    Completed with errs

--- main.js
+++ main.js
@@ -30,27 +30,60 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
-
+const movePiece = (startStack, endStack) => {
+  //Remove the end with POP to start another stack.
+let movedPiece = stacks[startStack].pop();
+// Now, PUSH the piece we held onto another stack.
+  stacks[endStack].push(movedPiece); 
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
-
+const isLegal = (startStack, endStack) => {
+  // We are are moving a piece from a stack that has pieces.
+if (stacks[startStack].length > 0) {
+  // now we can place any piece on an empty stack.
+  if (stacks[startStack].length == 0) {
+    return true;
+  }
+// this like makes sure the piece on the end stack is larger than the piece we mare moving  with SLICE.
+  if (stacks[endStack].slice(-1) > stacks[startStack].slice(-1)){
+    return true;
+    // if conditions are not met, the move is illegal
+  }else{
+    console.log("This is a invalid Move!")
+    return false;
+  }
+}
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
+// before anything else happens, make sure the game isnt already completed.
+// a win happens when all 4 blocks in the b or c stack are in order [4,3,2,1]
+
 const checkForWin = () => {
   // Your code here
-
+if (stacks['b'].length == 4 ) {
+  
+  return true;
+}
+if(stacks['c'].length == 4 ) {
+  
+  return true;
+}
+else {return false;}
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
-
+if (isLegal(startStack, endStack)) { 
+movePiece(startStack, endStack);
+// after moving the piece, check for win.
+if(checkForWin()) {
+  console.log("POGGERS! You've Won!")
+  return true;
+}else{ return false; }
+}else{ return false; }
 }
 
 const getPrompt = () => {
